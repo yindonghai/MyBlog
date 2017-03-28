64b069020431f908111449ae3671c5b2a4a4b8e1
yindonghai <1434577136@qq.com>
2017-03-28 19:53:11 +0800

    首页布局和样式修改

--- src/js/common.js
+++ src/js/common.js
@@ -1,16 +1,51 @@
+/**
+ * 菜单&搜索显示隐藏
+ */
+
+$('.header_lt button[data-ui-toggleClass]').on('click',function(){
+
+    var targetArr = $(this).attr('data-target').split(' ');
+    var classArr = $(this).attr('data-ui-toggleClass').split(' ');
+
+    for(var i=0; i<targetArr.length; i++){
+       $(targetArr[i]).toggleClass(classArr[i]);
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 天气插件调用
  */
-;$(function() {
+/*;$(function() {
     $('#weather').leoweather({
         format: '<i class="icon-{图标}">{夜间气温}℃-{白天气温}℃</i>' + '<p>{城市}<span> | </span>{天气}<span> | </span>{风向}{风级}级</p>' + '<p>{年}/{月}/{日}<span> | </span>周{周}</p>'
     });
-})
+})*/
 
 /**
  * swiper插件
  */
-;$(function() {
+/*;$(function() {
     var mySwiper = new Swiper('.swiper-container', {
         direction: 'horizontal',
         effect: 'fade',
@@ -28,6 +63,6 @@
         // 如果需要滚动条
         //scrollbar: '.swiper-scrollbar',
     })
-})
+})*/
 
 
