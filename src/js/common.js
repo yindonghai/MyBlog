6917816438e4b8c88d13d932f47f932ab1820107
yindonghai <1434577136@qq.com>
2017-03-31 09:56:19 +0800

    blog代码更新

--- src/js/common.js
+++ src/js/common.js
@@ -1,16 +1,42 @@
 /**
- * 菜单&搜索显示隐藏
+ * 左侧边栏&搜索显示隐藏
  */
 
-$('.header_lt button[data-ui-toggleClass]').on('click',function(){
+$('.header_lt button[data-ui-toggleClass]').on('click', function() {
 
     var targetArr = $(this).attr('data-target').split(' ');
     var classArr = $(this).attr('data-ui-toggleClass').split(' ');
 
-    for(var i=0; i<targetArr.length; i++){
-       $(targetArr[i]).toggleClass(classArr[i]);
+    for (var i = 0; i < targetArr.length; i++) {
+        $(targetArr[i]).toggleClass(classArr[i]);
     }
 
+});
+
+/**
+ * 菜单下拉
+ */
+$('.blog_nav>ul>li>a').on('click',function(ev){
+    //先隐藏其他子菜单
+    $(this).parent().siblings().find('ul').slideUp();
+    //判断是否有子菜单
+    var hasUl = $(this).siblings('ul').length;
+    if(hasUl){
+        $(this).children('i:last-child').toggleClass('icon-angle-down');
+        ev.preventDefault();  //阻止a跳转链接
+        $(this).siblings('ul').slideToggle();
+    }
+    
+
+});
+
+/*
+天气插件调用
+ */
+$(function() {
+    $('#weather').leoweather({
+        format: '<i class="icon-{图标}">{夜间气温}℃-{白天气温}℃</i>' + '<p>{城市}<span></span>{天气}<span></span>{风向}{风级}级</p>'
+    });
 });
 
 
@@ -64,5 +90,3 @@ $('.header_lt button[data-ui-toggleClass]').on('click',function(){
         //scrollbar: '.swiper-scrollbar',
     })
 })*/
-
-
